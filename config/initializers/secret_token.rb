522860ed7521769e85ee79d50d236e633018f0f2
Nourhan AbdelTawab <nourhan.abdeltawab@robustastudio.com>
2013-12-02 11:29:40 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Newspaper::Application.config.secret_token = 'f5c3c67d1690f512344c75850719850278fe1a8b06cdc3e00717e0ef4ac9c364ae5256d42c7514af9db7003fede58d996889dbafc32df46d8d9ced7e79792d38'
